7ba4aabcc085b77bd43485259a0f86ecd4290fef
eskatonic <brianmays@me.com>
2014-09-07 16:04:43 -0700

    Initial commit

---
+++ GitHubTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GitHubTest
+//
+//  Created by Brian on 9/7/14.
+//  Copyright (c) 2014 __BRIANMAYS__. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
